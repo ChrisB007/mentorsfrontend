59d309392320c4871429cf946192139d84455a9c
ChrisB007 <chrisbitoy@gmail.com>
2022-03-22 21:18:43 -0400

    more edit

--- components/Hero.js
+++ components/Hero.js
@@ -101,11 +101,11 @@ export default function Hero() {
         <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
           <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">Learn by Practicing.</span>
+              <span className="block xl:inline">Learn by Practicing</span>
             </h1>
             <h2>
               <span className="text-4xl font-bold text-lime-600">
-                ...With Experts.
+                With Experts Mentors
               </span>
             </h2>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
