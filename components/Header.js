64ae04a895726b00a4e5a6ae7c598ca5a21012b7
ChrisB007 <chrisbitoy@gmail.com>
2022-03-24 01:23:10 -0400

    updated Link

--- components/Header.js
+++ components/Header.js
@@ -349,20 +349,18 @@ export default function Header({ session }) {
             </Popover.Group>
             <div className="flex items-center md:ml-12">
               {!session && (
-                <a
-                  href="/login"
-                  className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700"
-                >
-                  Get Access
-                </a>
+                <Link href="/login">
+                  <a className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700">
+                    Get Access
+                  </a>
+                </Link>
               )}
               {session && (
-                <a
-                  href="/"
-                  className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700"
-                >
-                  Sign Out
-                </a>
+                <Link href="/dashboard">
+                  <a className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700">
+                    Sign Out
+                  </a>
+                </Link>
               )}
             </div>
           </div>
@@ -475,20 +473,18 @@ export default function Header({ session }) {
               </div>
               <div className="flex items-center md:ml-12">
                 {!session && (
-                  <a
-                    href="/login"
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    Get Access
-                  </a>
+                  <Link href="/login">
+                    <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                      Get Access
+                    </a>
+                  </Link>
                 )}
                 {session && (
-                  <a
-                    href="/"
-                    className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700"
-                  >
-                    Sign Out
-                  </a>
+                  <Link href="/dashboard">
+                    <a className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700">
+                      Sign Out
+                    </a>
+                  </Link>
                 )}
               </div>
             </div>
