caef4b1a79704d2a4aa4d26c1a8e4e4cb3802066
ChrisB007 <chrisbitoy@gmail.com>
2022-03-31 15:59:18 -0400

    updated nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -37,7 +37,7 @@ export default NextAuth({
     signOut: '/auth/signout',
     error: '/auth/error', // Error code passed in query string as ?error=
     verifyRequest: '/auth/verify-request', // (used for check email message)
-    newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
+    //    newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   secret: process.env.NEXT_PUBLIC_SECRET,
 });
