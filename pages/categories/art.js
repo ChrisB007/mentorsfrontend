04165b9a013aae5121306b01dd71f4963cf6d46c
ChrisB007 <chrisbitoy@gmail.com>
2022-03-31 13:00:08 -0400

    artJS updated

--- pages/categories/art.js
+++ pages/categories/art.js
@@ -1,31 +1,31 @@
-import { PrismaClient } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
-const Art = ({ data }) => {
+const Art = () => {
   return <div>Art</div>;
 };
 
-export async function getServerSideProps(context) {
-  if (req.method === 'GET') {
-    try {
-      // Get all subCategories
-      const subCategories = await prisma.subCategory.findMany();
-      return {
-        props: {
-          subCategories,
-        },
-      };
-    } catch (error) {
-      console.error(error);
-    }
-  }
+//export async function getServerSideProps(context) {
+//  if (req.method === 'GET') {
+//    try {
+//      // Get all subCategories
+//      const subCategories = await prisma.subCategory.findMany();
+//      return {
+//        props: {
+//          subCategories,
+//        },
+//      };
+//    } catch (error) {
+//      console.error(error);
+//    }
+//  }
 
-  return {
-    props: {
-      subCategories: [],
-    },
-  };
-}
+//  return {
+//    props: {
+//      subCategories: [],
+//    },
+//  };
+//}
 
 export default Art;
