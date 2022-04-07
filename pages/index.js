f5cf6e4e22b1368878ac2c127ebe9cd6ee0250b1
ChrisB007 <chrisbitoy@gmail.com>
2022-04-07 11:05:38 -0400

    temp fix for server error

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,20 @@ import Head from 'next/head';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Category from '../components/Categories';
+import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 
-export default function Home({ data }) {
-  const { categories } = data;
-  const catInfo = categories.map((category) => {
-    return {
-      id: category.id,
-      name: category.name,
-      url: category.url,
-      icon: category.icon,
-      subCategories: category.subcategories,
-    };
-  });
+export default function Home() {
+  //  const { categories } = data;
+  //  const catInfo = categories.map((category) => {
+  //    return {
+  //      id: category.id,
+  //      name: category.name,
+  //      url: category.url,
+  //      icon: category.icon,
+  //      subCategories: category.subcategories,
+  //    };
+  //  });
 
   //  console.log(catInfo);
 
@@ -33,7 +34,7 @@ export default function Home({ data }) {
           <main className="bg-gradient-to-r from-gray-50 via-slate-300 to-gray-50 pb-6">
             <Hero />
             <div className="grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4 px-3 max-w-7xl m-auto pb-3 mt-3">
-              {catInfo.map((category) => {
+              {/*{catInfo.map((category) => {
                 return (
                   <div key={category.id} className="">
                     <Category
@@ -46,7 +47,7 @@ export default function Home({ data }) {
                     />
                   </div>
                 );
-              })}
+              })}*/}
             </div>
             <Features />
           </main>
@@ -56,12 +57,37 @@ export default function Home({ data }) {
   );
 }
 
-export async function getServerSideProps() {
-  const categoryUrl = `http://localhost:3000/api/categories`;
-  //  const localUrl = `http://localhost:3000/api/categories`;
-  const { data } = await axios.get(categoryUrl);
+export async function getServerSideProps(req, res) {
+  const prisma = new PrismaClient();
+  try {
+    if (req.method === 'POST') {
+      const { name, description, url, icon } = req.body;
+
+      const categories = await prisma.category.createMany({
+        data: [{ name, url, description, icon }],
+      });
+      res.status(200).json({ categories });
+    }
+
+    const categories = await prisma.category.findMany({
+      include: {
+        subcategories: {
+          include: {
+            sub: true,
+          },
+        },
+      },
+    });
+    res.status(200).json({ categories });
+
+    console.log('CATEE', categories);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    prisma.$disconnect();
+  }
 
   return {
-    props: { data },
+    props: {},
   };
 }
