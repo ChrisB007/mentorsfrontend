639e7a4e6de51b32810cfa5b43e0aa683e4bfa0f
ChrisB007 <chrisbitoy@gmail.com>
2022-04-07 12:05:53 -0400

    fix for categories

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,8 @@ import Category from '../components/Categories';
 import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 
-export default function Home() {
-  //  const { categories } = data;
-  //  const catInfo = categories.map((category) => {
-  //    return {
-  //      id: category.id,
-  //      name: category.name,
-  //      url: category.url,
-  //      icon: category.icon,
-  //      subCategories: category.subcategories,
-  //    };
-  //  });
-
-  //  console.log(catInfo);
+export default function Home({ categories }) {
+  console.log(Array.isArray(categories));
 
   return (
     <>
@@ -34,7 +23,7 @@ export default function Home() {
           <main className="bg-gradient-to-r from-gray-50 via-slate-300 to-gray-50 pb-6">
             <Hero />
             <div className="grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4 px-3 max-w-7xl m-auto pb-3 mt-3">
-              {/*{catInfo.map((category) => {
+              {categories.map((category) => {
                 return (
                   <div key={category.id} className="">
                     <Category
@@ -47,7 +36,7 @@ export default function Home() {
                     />
                   </div>
                 );
-              })}*/}
+              })}
             </div>
             <Features />
           </main>
@@ -57,37 +46,37 @@ export default function Home() {
   );
 }
 
-export async function getServerSideProps(req, res) {
+export async function getServerSideProps() {
   const prisma = new PrismaClient();
-  try {
-    if (req.method === 'POST') {
-      const { name, description, url, icon } = req.body;
-
-      const categories = await prisma.category.createMany({
-        data: [{ name, url, description, icon }],
-      });
-      res.status(200).json({ categories });
-    }
-
-    const categories = await prisma.category.findMany({
-      include: {
-        subcategories: {
-          include: {
-            sub: true,
+  const categories = await prisma.category.findMany({
+    select: {
+      id: true,
+      name: true,
+      url: true,
+      icon: true,
+      subcategories: {
+        select: {
+          id: true,
+          name: true,
+          url: true,
+          icon: true,
+        },
+        select: {
+          sub: {
+            select: {
+              id: true,
+              name: true,
+              url: true,
+              image: true,
+              description: true,
+            },
           },
         },
       },
-    });
-    res.status(200).json({ categories });
-
-    console.log('CATEE', categories);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    prisma.$disconnect();
-  }
+    },
+  });
 
   return {
-    props: {},
+    props: { categories },
   };
 }
