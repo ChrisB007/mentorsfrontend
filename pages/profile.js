1df6985272c2428080fa1c3dfed57ae55382976c
ChrisB007 <chrisbitoy@gmail.com>
2022-04-19 19:04:54 -0400

    added public profile page

---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Details from '../components/Details';
+
+const Profile = () => {
+  return (
+    <>
+      <div>
+        <Details />
+      </div>
+    </>
+  );
+};
+
+export default Profile;
